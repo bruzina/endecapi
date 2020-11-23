cc08c1483f49f2966051bf28f3233737256d85d9
Martin Bruzina <martin@bruzina.cz>
2020-11-23 10:35:11 +0100

    Add springboot initializr stub

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Simple and small demo Springboot application providing API for simple ciphers.
 
+## Prerequisites
+
+```bash
+apt install -y openjdk-11-jre openjdk-11-jdk
+echo "JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64/" >> /etc/environment
+```
+
 ## License
 
 [Proprierary](LICENSE)
---
+++ src/main/java/cz/bruzina/endecapi/EndecapiApplication.java
@@ -0,0 +1,13 @@
+package cz.bruzina.endecapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndecapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndecapiApplication.class, args);
+	}
+
+}
